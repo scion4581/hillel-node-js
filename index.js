bfe03bc66d564c46d8bbf128b00ca20772780c2d
p.savostin <p.savostin@temabit.com>
2025-03-03 01:39:57 +0200

    homework #2

--- index.js
+++ index.js
@@ -4,4 +4,5 @@ const logger = new Logger();
 
 logger.info('Test info message');
 logger.warning('Test warning message');
-logger.error('Test error message');
+logger.error('Test error message')
+logger.info(new Error('Some error'));
